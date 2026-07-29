f9a4b0e6e38646ea3a3882e745a39cad5a76103f
S M Giyas Uddin <smgiyasuddin@Ss-MacBook-Pro.local>
2026-07-29 15:10:11 +0600

    add campaign

--- resources/views/backEnd/review/pending.blade.php
+++ resources/views/backEnd/review/pending.blade.php
@@ -76,19 +76,19 @@
 
 @section('script')
 <!-- third party js -->
-<script src="{{asset('backEnd/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.html5.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.flash.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.print.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/datatables.net-select/js/dataTables.select.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/pdfmake/build/pdfmake.min.js')}}">
-<script src="{{asset('backEnd/assets/libs/pdfmake/build/vfs_fonts.js')}}">
-<script src="{{asset('backEnd/assets/js/pages/datatables.init.js')}}">
+<script src="{{asset('backEnd/assets/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-bs5/js/dataTables.bootstrap5.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-responsive-bs5/js/responsive.bootstrap5.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-buttons-bs5/js/buttons.bootstrap5.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-keytable/js/dataTables.keyTable.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/datatables.net-select/js/dataTables.select.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/pdfmake/build/pdfmake.min.js')}}"></script>
+<script src="{{asset('backEnd/assets/libs/pdfmake/build/vfs_fonts.js')}}"></script>
+<script src="{{asset('backEnd/assets/js/pages/datatables.init.js')}}"></script>
 <!-- third party js ends -->
 @endsection
