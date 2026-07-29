e12d045a6f29a5f97d0e7efdc3267fbf1a4bb96d
S M Giyas Uddin <smgiyasuddin@Ss-MacBook-Pro.local>
2026-07-29 14:53:56 +0600

    add campaign

--- resources/views/backEnd/review/create.blade.php
+++ resources/views/backEnd/review/create.blade.php
@@ -1,9 +1,8 @@
 @extends('backEnd.layouts.master')
 @section('title', 'Review Create')
 @section('css')
-    <link href="{{ asset('backEnd }}/assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('backEnd }}/assets/libs/summernote/summernote-lite.min.css" rel="stylesheet"
-        type="text/css" />
+    <link href="{{ asset('backEnd/assets/libs/select2/css/select2.min.css')}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('backEnd/assets/libs/summernote/summernote-lite.min.css')}}" rel="stylesheet" />
 @endsection
 @section('content')
     <div class="container-fluid">
@@ -132,10 +131,10 @@
 
 
 @section('script')
-    <script src="{{ asset('backEnd }}/assets/libs/parsleyjs/parsley.min.js"></script>
-    <script src="{{ asset('backEnd }}/assets/js/pages/form-validation.init.js"></script>
-    <script src="{{ asset('backEnd }}/assets/libs/select2/js/select2.min.js"></script>
-    <script src="{{ asset('backEnd }}/assets/js/pages/form-advanced.init.js"></script>
+    <script src="{{ asset('backEnd/assets/libs/parsleyjs/parsley.min.js')}}"></script>
+    <script src="{{ asset('backEnd/assets/js/pages/form-validation.init.js')}}"></script>
+    <script src="{{ asset('backEnd/assets/libs/select2/js/select2.min.js')}}"></script>
+    <script src="{{ asset('backEnd/assets/js/pages/form-advanced.init.js')}}"></script>
     <!-- Plugins js -->
-    <script src="{{ asset('backEnd }}/assets/libs//summernote/summernote-lite.min.js"></script>
+    <script src="{{ asset('backEnd/assets/libs/summernote/summernote-lite.min.js')}}"></script>
 @endsection
